e6ffc9acf9ed7eb03b7d2d1e5ba6645cbc9ff597
Swapnil Arun Meshram <swapnilm1908@gmail.com>
2022-09-28 23:22:13 +0530

    .

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -6,7 +6,7 @@ import {
   addFavourite,
   removeFavourite,
 } from "../features/product/favouritSlice";
-
+//tumhara awaj nahi ara
 export const ProductCard: FC<{ product: productI }> = ({ product }) => {
   const [available, setAvailable] = useState<boolean>(false);
   const favouriteProduct = useAppSelector((state) => state.favourite.favourite);
@@ -15,19 +15,24 @@ export const ProductCard: FC<{ product: productI }> = ({ product }) => {
   useEffect(() => {
     if (favouriteProduct) {
       if (favouriteProduct.find((ele) => ele._id === product._id)) {
-        setAvailable(!available);
+        setAvailable(true);
       }
     }
   }, [favouriteProduct]);
+
+
   // add to Favourite
   const handleAddFavourite = () => {
     dispatch(addFavourite(product));
+    setAvailable(true);
     console.log(favouriteProduct)
   };
 
   // remove from favourite
   const handleRemoveFavourite = () => {
     dispatch(removeFavourite(product._id));
+    setAvailable(false);
+    console.log(favouriteProduct)
   };
 
   return (
@@ -62,7 +67,7 @@ export const ProductCard: FC<{ product: productI }> = ({ product }) => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-7 h-7 cursor-pointer text-red-500 hover:text-gray-300"
+            className="w-7 h-7 cursor-pointer text-red-500 "
           >
             <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
           </svg>
@@ -76,7 +81,7 @@ export const ProductCard: FC<{ product: productI }> = ({ product }) => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
-            className="w-7 h-7 cursor-pointer text-gray-400 hover:text-red-500"
+            className="w-7 h-7 cursor-pointer text-gray-400 "
           >
             <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
           </svg>
